187778df26d1db0890739f9b240513d53ab7167d
Nina Peire <nina.1.peire@kcl.ac.uk>
2022-11-22 15:31:29 +0000

    Add sign up form

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,21 @@
 """Forms of the project."""
+from django import forms
+from .models import Thing
 
-# Create your forms here.
+class ThingForm(forms.ModelForm):
+    class Meta:
+        model = Thing
+        fields = ['name', 'description', 'quantity']
+        widgets = {
+            'description': forms.Textarea(),
+            'quantity': forms.NumberInput(),
+        }
+
+    def save(self):
+        super().save(commit=False)
+        thing = Thing.objects.create_thing(
+            name = self.cleaned_data.get('name'),
+            description = self.cleaned_data.get('description'),
+            quantity = self.cleaned_data.get('quantity')
+        )
+        return thing
--- things/views.py
+++ things/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from .forms import ThingForm
 
 def home(request):
+    if request.method == 'POST':
+        form = ThingForm(request.POST)
+        if request.thing.is_authenticated:
+            current_thing = request.thing
+
+            if form.is_valid():
+                text = form.cleaned_data.get('text')
+                form.save(current_form)
+                #request_lesson = RequestLesson.objects.create()
+                return redirect('home')
+    else:
+        form = ThingForm()
     return render(request, 'home.html', {'form': form})
